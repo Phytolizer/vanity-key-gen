e135dac0b69b9e1e4b48000798b0bbae1eb3630b
Kyle Coffey <me@phytolizer.dev>
2022-02-05 17:19:48 -0600

    Clean up code.

--- src/main.cu
+++ src/main.cu
@@ -1,9 +1,1 @@
-__global__ void VecAdd(float *a, float *b, float *c) {
-  int i = threadIdx.x;
-  c[i] = a[i] + b[i];
-}
-
-int main() {
-  VecAdd<<<1, N>>>(a, b, c);
-  return 0;
-}
+int main(int argc, char **argv) { return 0; }
